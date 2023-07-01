42c0d578a5bc9a174ed047bf1e34356232305036
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-07-01 12:55:01 +0300

    add button style the button navigate the button to services

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-     mamboo vulae
     <Routes>
       <Route path='/' element={<Home />}></Route>
     </Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,16 @@ import React from 'react'
 function Home() {
   return (
     <div >
+
+<svg className='waves' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="rgb(244,192,53)" fill-opacity="1" d="M0,288L60,277.3C120,267,240,
+245,360,213.3C480,181,600,139,720,112C840,85,960,75,1080,106.7C1200,139,1320,213,
+1380,250.7L1440,288L1440,0L1380,0C1320,0,1200,0,1080,
+0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
+  </path>
+  </svg>
+
+  
         <div className='homepage'>
       <h2>Express yourself through Swahili dishes</h2>
       <div className='carousel'>
@@ -30,9 +40,7 @@ function Home() {
 
       </div>
       </div>
-      <div>
-        <button>Get Started</button>
-      </div>
+      <button className='BTN1'> Get Started</button>
     </div>
     
   )
